66d5acf699e9d3b3b262ced7b4ff7502e6fea208
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2022-06-30 18:40:26 -0300

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -32,7 +32,9 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-
+		
+    SUtil.gameCrashCheck();
+ 
 		if (stage != null)
 		{
 			init();
@@ -66,6 +68,8 @@ class Main extends Sprite
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
+
+		SUtil.doTheCheck();
 
 		#if !debug
 		initialState = TitleState;
