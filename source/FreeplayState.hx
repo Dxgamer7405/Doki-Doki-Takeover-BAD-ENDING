c271a9340afea6dfdad8301264dd989e17b679f8
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2022-06-30 19:01:37 -0300

    update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -200,6 +200,11 @@ class FreeplayState extends MusicBeatState
 		text.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, RIGHT);
 		text.scrollFactor.set();
 		add(text);
+		
+		#if android
+		addVirtualPad(FULL, A_B);
+		#end
+
 		super.create();
 	}
 
